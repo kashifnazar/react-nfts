c7e8f15fbbe09b1d70d611c7cd4fe34b4738cf09
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2023-03-07 19:30:20 +0000

    Handling loading and error cases

--- src/hooks/useGrid.tsx
+++ src/hooks/useGrid.tsx
@@ -10,9 +10,13 @@ type Props<T, R> = {
 export const useGrid = <T, R>({url, getList, renderItem}: Props<T, R>) => {
 
     const [data, setData] = useState<Array<T>>([])
+    const [isProgressBar, setIsProgressBar] = useState(false)
+    const [error, setError] = useState<Error | null>(null)
 
     const getNFTs = async () => {
-        
+
+        try{
+        setIsProgressBar(true)
         const response = await fetch(url, {
             headers: {
                 "X-API-KEY": "lart9HUN3wggl7fXvAwZNXOTO8J7azv3J9RvQuhTbmj0WcoGRwpfYYfMukzXKAD9"
@@ -21,12 +25,21 @@ export const useGrid = <T, R>({url, getList, renderItem}: Props<T, R>) => {
         
         const data = (await response.json()) as R
         setData(getList(data))
+        } catch(e) {
+            setError(e as Error)
+        } finally {
+            setIsProgressBar(false)
+        }
+
     }
 
     useEffect(() => {
        getNFTs()
     }, [])
 
-    return <Grid<T> data={data} resolver={(item: T) => renderItem(item)}/>
+    return isProgressBar ? 
+            <div className="flex justify-center">Please wait...</div> : 
+            error ? <div className="flex justify-center text-red-500">{error.message}</div>  :
+            <Grid<T> data={data} resolver={(item: T) => renderItem(item)}/>
 
 }
